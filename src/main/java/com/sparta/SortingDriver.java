3ec85bd2a9ee8234bdf36038c2e988272b6919a5
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2021-12-02 00:06:18 +0000

    Tidy main method

--- src/main/java/com/sparta/SortingDriver.java
+++ src/main/java/com/sparta/SortingDriver.java
@@ -21,30 +21,6 @@ public class SortingDriver {
 
         TimeReport.GenerateTimeReport();
 
-
-//        //Tree sort testing
-//        Integer [] arr = arrayIntegerGenerator.generateArray();
-//        System.out.println(Arrays.toString(arr));
-//        TreeSortImpl treeSort = new TreeSortImpl();
-//        treeSort.sortingAlgorithm(arr);
-//        System.out.println(Arrays.toString(arr));
-//
-//
-//        List<Integer> list = new ArrayList<>(List.of(arrayIntegerGenerator.generateArray()));
-//        System.out.println(list);
-//        TreeSortImpl treeSortList = new TreeSortImpl();
-//        treeSortList.sortingAlgorithm(list);
-//        System.out.println(list);
-//
-//
-//        String [] arrS = arrayStringGenerator.generateArray();
-//        System.out.println(Arrays.toString(arrS));
-//        TreeSortImpl treeSort2 = new TreeSortImpl();
-//        treeSort2.sortingAlgorithm(arrS);
-//        System.out.println(Arrays.toString(arrS));
-
-
-
     }
 
 }
